1425feb387d0e4097ecb31b623531003c53a3c2f
Gary Marigliano <gmarigliano93@gmail.com>
2016-06-07 10:30:38 +0200

    Add user toto and grant remote access

--- mariadb/scripts/create_db.sh
+++ mariadb/scripts/create_db.sh
@@ -1,11 +1,20 @@
 #/bin/ash
+
+# Create db user
+mysql -uroot -palpine -e "CREATE USER 'toto'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -palpine -e "GRANT ALL PRIVILEGES ON *.* TO 'toto'@'localhost' WITH GRANT OPTION;"
+mysql -uroot -palpine -e "CREATE USER 'toto'@'%' IDENTIFIED BY 'password';"
+mysql -uroot -palpine -e "GRANT ALL PRIVILEGES ON *.* TO 'toto'@'%' WITH GRANT OPTION;"
+
+# Create db
 mysql -uroot -palpine -e "CREATE DATABASE todo_list COLLATE 'utf8_general_ci';"
-mysql -uroot -palpine -e "GRANT ALL ON todo_list.* TO user@localhost IDENTIFIED BY 'password';"
 mysql -uroot -palpine -e "CREATE TABLE todo_list.tbl_tasks (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title varchar(50) NOT NULL,
   description varchar(255) NOT NULL,
   done tinyint(1) NOT NULL
 ) COMMENT='' ENGINE='InnoDB';"
+
+# Fill db with default entry
 mysql -uroot -palpine -e "INSERT INTO todo_list.tbl_tasks (title, description, done)
 VALUES ('My first task', 'This is my first task', '0');"
