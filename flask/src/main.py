fc8c56977cc48f5ef7e0bf74f443170b687c959e
Gary Marigliano <gmarigliano93@gmail.com>
2016-06-07 14:13:47 +0200

    Add MySQL communication in Flask

--- flask/src/main.py
+++ flask/src/main.py
@@ -1,16 +1,29 @@
 #!usr/bin/python3
 # coding: utf-8
 from flask import Flask, jsonify, abort, request, make_response, url_for
+import MySQLdb as mdb
 
-app = Flask(__name__, static_url_path ="")
+app = Flask(__name__, static_url_path="")
+
+MYSQL_USERNAME = "toto"
+MYSQL_PASSWORD = "password"
+MYSQL_HOST = "mysqlsrv"
+MYSQL_DB = "todo_list"
+TASKS_TABLE = "tbl_tasks"
+
+# MySQL configurations
+
+con = mdb.connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DB)
+
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 tasks = [
     {
@@ -27,27 +40,45 @@ tasks = [
     }
 ]
 
+
 def make_public_task(task):
     new_task = {}
     for field in task:
         if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id = task['id'], _external = True)
+            new_task['uri'] = url_for('get_task', task_id=task[
+                                      'id'], _external=True)
         else:
             new_task[field] = task[field]
     return new_task
 
-@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
-def get_tasks():
-    return jsonify( { 'tasks': list(map(make_public_task, tasks)) } )
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    # db.query("""SELECT * FROM %s""" % TASKS_TABLE)
+    # r = db.use_result()
+    # tasks = r.fetch_row()
+    # return jsonify({"tasks": str(tasks)})
+    with con:
+        cur = con.cursor()
+        sql = """SELECT * FROM %s""" % TASKS_TABLE
+        cur.execute(sql)
+
+        results = cur.fetchall()
+        return jsonify({"tasks": str(results)})
+        # return jsonify({'tasks': list(map(make_public_task, results))})
+
+    abort(400)
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = list(filter(lambda t: t['id'] == task_id, tasks))
     if len(task) == 0:
         abort(404)
-    return jsonify( { 'task': make_public_task(task[0]) } )
+    return jsonify({'task': make_public_task(task[0])})
 
-@app.route('/todo/api/v1.0/tasks', methods = ['POST'])
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
@@ -58,9 +89,10 @@ def create_task():
         'done': False
     }
     tasks.append(task)
-    return jsonify( { 'task': make_public_task(task) } ), 201
+    return jsonify({'task': make_public_task(task)}), 201
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['PUT'])
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = list(filter(lambda t: t['id'] == task_id, tasks))
     if len(task) == 0:
@@ -74,17 +106,19 @@ def update_task(task_id):
     if 'done' in request.json and not isinstance(request.json['done'], bool):
         abort(400)
     task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['description'] = request.json.get(
+        'description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify( { 'task': make_public_task(task[0]) } )
+    return jsonify({'task': make_public_task(task[0])})
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['DELETE'])
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
     task = list(filter(lambda t: t['id'] == task_id, tasks))
     if len(task) == 0:
         abort(404)
     tasks.remove(task[0])
-    return jsonify( { 'result': True } )
+    return jsonify({'result': True})
 
 
 if __name__ == '__main__':
