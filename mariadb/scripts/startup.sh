2dbc6aec7ad4bbc57bde6ae2b0731bcb8135c44a
Gary Marigliano <gmarigliano93@gmail.com>
2016-06-07 19:05:39 +0200

    Remove dead code in startup.sh

--- mariadb/scripts/startup.sh
+++ mariadb/scripts/startup.sh
@@ -1,9 +1,4 @@
 #!/bin/ash
-# if [[ `ls /var/lib/mysql | wc -l`  == "0" ]]; then
-#   /usr/bin/mysql_install_db --user=mysql
-# fi
-# /usr/bin/mysqld_safe --datadir="/var/lib/mysql"
-
 if [[ `ls /var/lib/mysql | wc -l`  == "0" ]]; then
   mysql_install_db --user=mysql
   mysqld_safe --datadir="/var/lib/mysql" &
