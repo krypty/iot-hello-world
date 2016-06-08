2fe2c199125ad4ae15b38592183152bdc2811245
Gary Marigliano <gmarigliano93@gmail.com>
2016-06-08 19:54:47 +0000

    Remove useless capabilities for mariadb

--- mariadb/start.sh
+++ mariadb/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --disable-content-trust -d --name "mysqlsrv" --env-file="../db_config.env" -v /tmp/toto/:/var/lib/mysql mysql-alpine
+docker run --disable-content-trust -d --cap-drop=all --cap-add={SETGID,SETUID,CHOWN,DAC_OVERRIDE,KILL,FOWNER} --name "mysqlsrv" --env-file="../db_config.env" -v /tmp/toto/:/var/lib/mysql mysql-alpine
