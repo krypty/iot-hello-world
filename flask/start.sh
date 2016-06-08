5ebd938090db1532ca26c77038f95b5e032fa55a
Gary Marigliano <gmarigliano93@gmail.com>
2016-06-08 22:37:16 +0200

    Add memory limit for flask's container

--- flask/start.sh
+++ flask/start.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-docker run --disable-content-trust -it --cap-drop=all -p 5000:5000 --link mysqlsrv:mysqlsrv --rm --env-file="../db_config.env" iot-hello-world
+docker run --disable-content-trust -it --cap-drop=all --memory=128MB -p 5000:5000 --link mysqlsrv:mysqlsrv --rm --env-file="../db_config.env" iot-hello-world
