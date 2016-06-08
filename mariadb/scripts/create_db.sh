af9b90191104f20067828bf81ba04cdbe730e056
Gary Marigliano <gmarigliano93@gmail.com>
2016-06-08 18:18:06 +0000

    Use env vars in create_db.sh

--- mariadb/scripts/create_db.sh
+++ mariadb/scripts/create_db.sh
@@ -7,8 +7,8 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_USERNAME'@'%' IDENTI
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USERNAME'@'%' WITH GRANT OPTION;"
 
 # Create db
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE todo_list COLLATE 'utf8_general_ci';"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE TABLE todo_list.tbl_tasks (
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DB COLLATE 'utf8_general_ci';"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE TABLE $MYSQL_DB.$TASKS_TABLE (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title varchar(50) NOT NULL,
   description varchar(255) NOT NULL,
@@ -16,6 +16,6 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE TABLE todo_list.tbl_tasks (
 ) COMMENT='' ENGINE='InnoDB';"
 
 # Fill db with default entry
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO todo_list.tbl_tasks (title, description, done) VALUES ('My first task', 'This is my first task', '0');"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO $MYSQL_DB.$TASKS_TABLE (title, description, done) VALUES ('My first task', 'This is my first task', '0');"
 
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO todo_list.tbl_tasks (title, description, done) VALUES ('My second task', 'This is my second task', '1');"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "INSERT INTO $MYSQL_DB.$TASKS_TABLE (title, description, done) VALUES ('My second task', 'This is my second task', '1');"
